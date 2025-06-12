e6f0e45a85e1cc2739e964cb6af3e42fccbd7947
ShamutdinovD <shamsutdinov13051997@gmail.com>
2025-06-12 17:41:38 +0300

    init commit

---
+++ src/main/java/kfc/Person.java
@@ -0,0 +1,58 @@
+package kfc;
+
+public class Person {
+
+    private String fullName;
+
+    private int age;
+
+    int id;
+
+    int counter = 0;
+
+
+    public Person(){
+
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        if (fullName == null || fullName.isEmpty()) {
+            System.out.println("Имя не может быть пустым или null");
+            return;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            System.out.println("Возраст не может быть меньше 0");
+            return;
+        }
+    }
+
+    public Person(String fullName, int age, int id) {
+        this.fullName = fullName;
+        this.age = age;
+        this.id = counter++;
+    }
+
+    public void talk(){
+        if (id == 2) {
+            System.out.println("Привет я, " + fullName + " избранный!");
+        }
+        else if (fullName != null && !fullName.isEmpty() && age > 0) {
+            System.out.println("Привет я " + fullName + " и мне " + age + " лет");
+        }
+    }
+
+    public void count(){
+        System.out.println("Количество созданных объектов " + counter);
+    }
+}
