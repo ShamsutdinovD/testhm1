3cb9c16b7207e377f44e10498c361de90c2fd49e
ShamutdinovD <shamsutdinov13051997@gmail.com>
2025-06-12 17:41:31 +0300

    init commit

--- src/main/java/kfc/Main.java
+++ src/main/java/kfc/Main.java
@@ -1,29 +1,15 @@
 package kfc;
 
-import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] array = new int[20];
-        int sum = 0;
-        int max = array[0];
 
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = new Random().nextInt(20);
-            System.out.print(array[i] + " ");
-            sum = sum + array[i];
-        }
-
-        System.out.println("\nСумма всех элементов: " + sum);
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-
-            System.out.println("\nНаибольший элемент массива: " + max);
+    public static void main(String[] arg) {
+        Person person = new Person("Динис", 28, 2);
+        person.talk();
+        person.setFullName("Иван");
+        person.setAge(-1);
+        person.count();
 
     }
 }
